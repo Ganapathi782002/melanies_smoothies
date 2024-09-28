fc2d3b1ceb3a612c68eaed905916b001f442412e
Ganapathi <75827414+Ganapathi782002@users.noreply.github.com>
2024-09-28 17:53:04 +0530

    Update streamlit_app.py
    Converting streamlit's dataframe to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -16,7 +17,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 # Get available fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # Select ingredients for the smoothie
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe,max_selections = 5)
